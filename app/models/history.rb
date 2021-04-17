c2cce606358d0007b506de194f6c6c248b502525
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-18 05:56:05 +0900

    User登録テストコード

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,10 +1,10 @@
 class History < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '１年未満' },
-    { id: 3, name: '１年以上３年未満' },
-    { id: 4, name: '４年以上' },
-    { id: 5, name: '上級者' },
+    { id: 0, name: '--' },
+    { id: 1, name: '１年未満' },
+    { id: 2, name: '１年以上３年未満' },
+    { id: 3, name: '４年以上' },
+    { id: 4, name: '上級者' },
   ]
 
   include ActiveHash::Associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :instrument
-    validates :prefecture_id
-    validates :history_id
+    validates :prefecture_id,numericality: { other_than: 0 }
+    validates :history_id,numericality: { other_than: 0 }
+    validates :age, format:{with: /\A[0-9]+\z/,message:'半角数字のみで入力してください'}
     validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: '全て半角で英数字を混ぜて設定してください' }
   end
+
+    
 
 end
