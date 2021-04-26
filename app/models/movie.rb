70a57a1da83c14c37ecd1ec2e75aeb4980eef915
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-26 18:05:37 +0900

    投稿削除時のコメント削除　ビュー修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,7 +11,7 @@ class Movie < ApplicationRecord
 
   # has_one_attached :video_top
   belongs_to :user
-  has_many :comments
+  has_many :comments,dependent: :destroy
   has_many :likes, dependent: :destroy
 
 end
