d29855c6dc0ee11af032f4c223b7b39d7cf758be
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-27 20:19:11 +0900

    DM相手一覧表示　実装（リンクなし）

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -28,25 +28,41 @@ class ChatsController < ApplicationController
   end
 
   def index
-    # user_rooms=UserRoom.where(user_id: params[:user_id])
-    # @u=[]
-    # user_rooms.each do |user_room|
-    #   @users=User.joins(:user_rooms).where(room_id: user_room.room_id)
-    #   @u.push(@users)
-    # end
-
-    
+   
     user_rooms=UserRoom.where(user_id: params[:user_id])
-    @cui=[]
+    a=[]
     user_rooms.each do |user_room|
-    chat_user=UserRoom.where(room_id: params[user_room.room_id]).where.not(user_id: params[:user_id])
-    @cui.push(chat_user)
+      a.push(user_room.room_id)
+    end
+
+    b=[]
+    a.each do |r_id|
+    chat_user=UserRoom.where(room_id: r_id).where.not(user_id: params[:user_id])
+    b.push(chat_user)
     end
     
-
-    chat_user_ids.each do |chat_user_id| 
-      @chat_user=User.where(id: chat_user_id.user_id)
+    array_nums=b.size
+    c=[]
+    array_nums.times do |num|
+    chat_user_ids=b[num][0].user_id
+    c.push(chat_user_ids)
     end
+    
+    d=[]
+    c.each do|user_id|
+    user=User.find(user_id)
+    d.push(user)
+    end
+
+    d_array_nums=d.size
+    e=[]
+    d_array_nums.times do |d_array_num|
+      chat_user_names = d[d_array_num].nickname
+      e.push(chat_user_names)
+    end
+    
+    @dm_user_names=e
+
   end
 
   private
